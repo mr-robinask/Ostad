611570ce4cc0e1f4577b3433fd849dd59bdf0a20
mr-robinask <87421655+mr-robinask@users.noreply.github.com>
2025-07-31 23:41:28 +0600

    Update admin.py

--- Exam Week 3/job_portal/jobs/admin.py
+++ Exam Week 3/job_portal/jobs/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import UserProfile, Job, Application
+from .models import Job, Application, UserProfile
 
-admin.site.register(UserProfile)
-admin.site.register(Job)
-admin.site.register(Application)
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company_name', 'location', 'posted_by', 'created_at')
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('job', 'applicant', 'applied_at', 'status')
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
