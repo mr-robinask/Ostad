fbae2af1c38552809269d4ce5a22f0fb4dd0ab0f
mr-robinask <87421655+mr-robinask@users.noreply.github.com>
2025-10-26 00:03:47 +0600

    Update serializers.py

--- Final Exam/blood_management_system/users/serializers.py
+++ Final Exam/blood_management_system/users/serializers.py
@@ -5,10 +5,6 @@ class DonorProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = DonorProfile
         fields = ['blood_group', 'phone_number', 'location', 'is_available']
-        extra_kwargs = {
-            'blood_group': {'required': True},
-            'phone_number': {'required': True},
-        }
 
 class UserSerializer(serializers.ModelSerializer):
     profile = DonorProfileSerializer()
@@ -33,7 +29,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             email=validated_data.get('email', ''),
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', ''),
-            role=CustomUser.Role.DONOR
+            role=CustomUser.Role.DONOR # API registration is for Donors
         )
         DonorProfile.objects.create(user=user, **profile_data)
         return user
