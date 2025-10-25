621d3870ff125cc6d49b54ecb1cd8473f6a36cfc
mr-robinask <87421655+mr-robinask@users.noreply.github.com>
2025-10-25 23:46:05 +0600

    Update api_views.py

--- Final Exam/blood_management_system/users/api_views.py
+++ Final Exam/blood_management_system/users/api_views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics, permissions
-from rest_framework.response import Response
 from .serializers import UserRegistrationSerializer, DonorProfileSerializer
 from .models import DonorProfile
 from django_filters.rest_framework import DjangoFilterBackend
