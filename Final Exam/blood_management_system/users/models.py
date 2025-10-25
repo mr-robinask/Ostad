fd82f2e6430842ba152e9956fafc930051e2ebf7
mr-robinask <87421655+mr-robinask@users.noreply.github.com>
2025-10-25 23:43:37 +0600

    Update models.py

--- Final Exam/blood_management_system/users/models.py
+++ Final Exam/blood_management_system/users/models.py
@@ -7,15 +7,16 @@ class CustomUser(AbstractUser):
         DONOR = "DONOR", "Donor"
 
     role = models.CharField(max_length=50, choices=Role.choices, default=Role.DONOR)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
 class DonorProfile(models.Model):
     BLOOD_GROUPS = [
         ('A+', 'A+'), ('A-', 'A-'), ('B+', 'B+'), ('B-', 'B-'),
-        ('AB+', 'AB+'), ('AB-','A-'), ('O+', 'O+'), ('O-', 'O-'),
+        ('AB+', 'AB+'), ('AB-', 'AB-'), ('O+', 'O+'), ('O-', 'O-'),
     ]
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="profile")
     blood_group = models.CharField(max_length=3, choices=BLOOD_GROUPS)
-    phone_number = models.CharField(max_length=15, blank=True)
+    phone_number = models.CharField(max_length=15)
     location = models.CharField(max_length=100, blank=True)
     is_available = models.BooleanField(default=True)
 
